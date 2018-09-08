ef883b8d2529445ffd9c199f0d8f794ba5258383
Philip du Toit <philip.dutoit@nutonomy.com>
2018-09-08 15:53:48 +0800

    ready for final train

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,8 +2,7 @@ from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.model = load_model('TLD_simulator.h5')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +15,32 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+
+
+        ##################################################
+      
+        # TODO:  Make sure that "image" is in the correct format expected by predict (should it be in a list, RGB vs BGR, etc.)
+
+        prediction = self.model.predict(image)
+
+        # {'unknown': 2, 'green': 0, 'yellow': 3, 'red': 1}, i.e. alphabetical
+
+
+        # uint8 UNKNOWN=4
+        # uint8 GREEN=2
+        # uint8 YELLOW=1
+        # uint8 RED=0
+
+        if prediction==0:
+          return TrafficLight.GREEN 
+        elif prediction==1:
+          return TrafficLight.RED 
+        elif prediction==2:
+          return TrafficLight.UNKNOWN 
+        elif prediction==3
+          return TrafficLight.YELLOW
+
+        #################################################
+
+
         return TrafficLight.UNKNOWN
